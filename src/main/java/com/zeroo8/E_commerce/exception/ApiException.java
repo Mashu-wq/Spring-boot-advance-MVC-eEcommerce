cee1844e5e33a6325dd0c3bf2ae49b799d8eacf9
Mayesha Marzia Zaman <mim2007088@stud.kuet.ac.bd>
2025-03-10 01:47:20 -0700

    created API exception to handle exceptions in the API

--- src/main/java/com/zeroo8/E_commerce/exception/ApiException.java
+++ src/main/java/com/zeroo8/E_commerce/exception/ApiException.java
@@ -1,5 +1,5 @@
 package com.zeroo8.E_commerce.exception;
-
+// ApiException class is created to handle exceptions in the API
 public class ApiException extends RuntimeException{
     private static final long serialVersionUIO = 1L;
 
