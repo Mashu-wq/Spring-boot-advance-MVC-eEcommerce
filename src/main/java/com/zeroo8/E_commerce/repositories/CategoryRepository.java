782814d6c3e40280e07ef137ebee985b6fac24ed
Mayesha Marzia Zaman <mim2007088@stud.kuet.ac.bd>
2025-03-10 01:14:05 -0700

    created categoryRepositories in repository package

--- src/main/java/com/zeroo8/E_commerce/repositories/CategoryRepository.java
+++ src/main/java/com/zeroo8/E_commerce/repositories/CategoryRepository.java
@@ -4,5 +4,6 @@ import com.zeroo8.E_commerce.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    
     Category findByCategoryName(String categoryName);
 }
