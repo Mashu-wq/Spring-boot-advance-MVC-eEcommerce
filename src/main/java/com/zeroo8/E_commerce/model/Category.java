fa2eb0dd0b1f42eefde6e43b6d05bd7eb4928a0c
Mayesha Marzia Zaman <mim2007088@stud.kuet.ac.bd>
2025-03-13 02:34:55 -0700

    Added get products method in Category.java

--- src/main/java/com/zeroo8/E_commerce/model/Category.java
+++ src/main/java/com/zeroo8/E_commerce/model/Category.java
@@ -1,16 +1,11 @@
 package com.zeroo8.E_commerce.model;
 
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-
+import java.util.List;
 
 @Entity(name = "categories")
-
 public class Category {
 
     @Id
@@ -18,17 +13,23 @@ public class Category {
     private Long categoryId;
 
     @NotBlank
-    @Size(min = 5, message = "Category name must contain atleast 5 characters")
+    @Size(min = 5, message = "Category name must contain at least 5 characters")
     private String categoryName;
 
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Product> products;
+
+    // No-argument constructor
+    public Category() {
+    }
+
+    // Parameterized constructor
     public Category(Long categoryId, String categoryName) {
         this.categoryId = categoryId;
         this.categoryName = categoryName;
     }
-    public Category(){
 
-    }
-
+    // Getters and Setters
     public Long getCategoryId() {
         return categoryId;
     }
@@ -43,5 +44,23 @@ public class Category {
 
     public void setCategoryName(String categoryName) {
         this.categoryName = categoryName;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    // toString() method for debugging/logging
+    @Override
+    public String toString() {
+        return "Category{" +
+                "categoryId=" + categoryId +
+                ", categoryName='" + categoryName + '\'' +
+                ", products=" + products +
+                '}';
     }
 }
