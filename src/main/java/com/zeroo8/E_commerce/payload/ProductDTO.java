dd0d60e30c6c92132ab42bcc737ae53df4f98e9b
Mayesha Marzia Zaman <mim2007088@stud.kuet.ac.bd>
2025-03-13 02:36:03 -0700

    Created ProductDTO class

---
+++ src/main/java/com/zeroo8/E_commerce/payload/ProductDTO.java
@@ -0,0 +1,108 @@
+package com.zeroo8.E_commerce.payload;
+
+public class ProductDTO {
+    private Long productId;
+    private String productName;
+    private String image;
+    private String description;
+    private Integer quantity;
+    private double price;
+    private double discount;
+    private double specialPrice;
+
+    // No-argument constructor
+    public ProductDTO() {
+    }
+
+    // Parameterized constructor
+    public ProductDTO(Long productId, String productName, String image, String description, Integer quantity, double price, double discount, double specialPrice) {
+        this.productId = productId;
+        this.productName = productName;
+        this.image = image;
+        this.description = description;
+        this.quantity = quantity;
+        this.price = price;
+        this.discount = discount;
+        this.specialPrice = specialPrice;
+    }
+
+    // Getters and Setters
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public double getSpecialPrice() {
+        return specialPrice;
+    }
+
+    public void setSpecialPrice(double specialPrice) {
+        this.specialPrice = specialPrice;
+    }
+
+    // toString() method for debugging/logging
+    @Override
+    public String toString() {
+        return "ProductDTO{" +
+                "productId=" + productId +
+                ", productName='" + productName + '\'' +
+                ", image='" + image + '\'' +
+                ", description='" + description + '\'' +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                ", discount=" + discount +
+                ", specialPrice=" + specialPrice +
+                '}';
+    }
+}
