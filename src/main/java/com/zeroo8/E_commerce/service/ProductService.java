b8fdd78040801481c8e5107767dac7f8870dbe5f
Mayesha Marzia Zaman <mim2007088@stud.kuet.ac.bd>
2025-03-13 02:39:09 -0700

    Implemented product Service with getAllProducts, searchByCategory, searchProductByKeyword, updateProduct, deleteProduct, updateProductImage

---
+++ src/main/java/com/zeroo8/E_commerce/service/ProductService.java
@@ -0,0 +1,24 @@
+package com.zeroo8.E_commerce.service;
+
+import com.zeroo8.E_commerce.payload.ProductDTO;
+import com.zeroo8.E_commerce.payload.ProductResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface ProductService {
+    ProductDTO addProduct(Long categoryId, ProductDTO product);
+
+    ProductResponse getAllProducts(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
+
+    ProductResponse searchByCategory(Long categoryId, Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
+
+    ProductResponse searchProductByKeyword(String keyword, Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
+
+    ProductDTO updateProduct(Long productId, ProductDTO product);
+
+    ProductDTO deleteProduct(Long productId);
+
+    ProductDTO updateProductImage(Long productId, MultipartFile image) throws IOException;
+}
+
