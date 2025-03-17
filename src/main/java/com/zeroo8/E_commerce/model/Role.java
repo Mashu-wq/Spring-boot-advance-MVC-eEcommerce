64227c462cd454d2e1ca14cd46ab3883dee7ec01
Mayesha Marzia Zaman <mim2007088@stud.kuet.ac.bd>
2025-03-17 02:57:57 -0700

    created Role.java class

---
+++ src/main/java/com/zeroo8/E_commerce/model/Role.java
@@ -0,0 +1,52 @@
+package com.zeroo8.E_commerce.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer roleId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, name = "role_name")
+    private AppRole roleName;
+
+    // Default constructor
+    public Role() {
+    }
+
+    // Parameterized constructor
+    public Role(AppRole roleName) {
+        this.roleName = roleName;
+    }
+
+    // Getters and Setters
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public AppRole getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(AppRole roleName) {
+        this.roleName = roleName;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Role{" +
+                "roleId=" + roleId +
+                ", roleName=" + roleName +
+                '}';
+    }
+}
